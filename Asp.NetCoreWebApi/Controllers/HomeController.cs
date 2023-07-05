c9275cec21015f3a4eadbb1c7a0234e602041584
Mehmet Can KALABAŞ <mehmetcankalabas.mk@gmail.com>
2023-07-05 15:02:30 +0300

    Add project filess.

---
+++ Asp.NetCoreWebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Asp.NetCoreWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            var result = new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello"
+            };
+            return Ok(result);
+        }
+    }
+}
